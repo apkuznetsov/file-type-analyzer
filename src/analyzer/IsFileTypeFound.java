f2257716eb7af2b695137b3e80a75014cee30f7f
Andrey <kouznts@ya.ru>
2020-07-22 13:20:36 +0400

    impl is file type found class

---
+++ src/analyzer/IsFileTypeFound.java
@@ -0,0 +1,22 @@
+package analyzer;
+
+import org.jetbrains.annotations.NotNull;
+
+public class IsFileTypeFound {
+
+    private final String fileName;
+    private final boolean isTypeFound;
+
+    public IsFileTypeFound(@NotNull String fileName, boolean isTypeFound) {
+        this.fileName = fileName;
+        this.isTypeFound = isTypeFound;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public boolean isTypeFound() {
+        return isTypeFound;
+    }
+}
