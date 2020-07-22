7e0b8ab68149ccd9667a2ef85ef7f89c931fd3cc
Andrey <kouznts@ya.ru>
2020-07-22 13:29:21 +0400

    remake analyzer

--- src/analyzer/FileTypeAnalyzer.java
+++ src/analyzer/FileTypeAnalyzer.java
@@ -2,35 +2,63 @@ package analyzer;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import static analyzer.KnuthMorrisPrattAlg.searchOccurrencesByKmpAlg;
 
 public class FileTypeAnalyzer {
 
-    public static boolean isTypeFound(@NotNull final String algName,
-                                      @NotNull final String fileName,
-                                      @NotNull final String pattern) {
+    public static List<IsFileTypeFound> isTypeFound(@NotNull final String algName,
+                                                    @NotNull final String folderName,
+                                                    @NotNull final String pattern) {
 
-        boolean isTypeFound = false;
+        final ExecutorService executor = Executors.newCachedThreadPool();
+        final File[] files = new File(folderName).listFiles();
+        final List<Future<IsFileTypeFound>> futures = new LinkedList<>();
 
-        try (InputStream input = new BufferedInputStream(
-                new FileInputStream(fileName)
-        )) {
+        for (File file : files) {
+            futures.add(
+                    executor.submit(() -> {
+                        boolean isTypeFound = false;
 
-            String fileContent = new String(input.readAllBytes());
+                        try (InputStream input = new BufferedInputStream(
+                                new FileInputStream(file)
+                        )) {
 
-            if (algName.equals("--naive")) {
-                isTypeFound = fileContent.contains(pattern);
-            } else if (algName.equals("--KMP")) {
-                isTypeFound = KnuthMorrisPrattAlg.searchOccurrencesByKmpAlg(fileContent, pattern).size() > 0;
-            }
+                            String fileContent = new String(input.readAllBytes());
 
-        } catch (IOException exc) {
-            exc.printStackTrace();
+                            if (algName.equals("--naive")) {
+                                isTypeFound = fileContent.contains(pattern);
+                            } else if (algName.equals("--KMP")) {
+                                isTypeFound = searchOccurrencesByKmpAlg(fileContent, pattern).size() > 0;
+                            }
+
+                        } catch (IOException exc) {
+                            exc.printStackTrace();
+                        }
+
+                        return new IsFileTypeFound(file.getName(), isTypeFound);
+                    })
+            );
         }
 
-        return isTypeFound;
+        List<IsFileTypeFound> result = new ArrayList<>(futures.size());
+        for (Future<IsFileTypeFound> future : futures) {
+            try {
+                result.add(future.get());
+            } catch (InterruptedException | ExecutionException exc) {
+                exc.printStackTrace();
+            }
+        }
+
+        executor.shutdown();
+        return result;
     }
 }
